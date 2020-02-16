f024c49c5e3d812f272ba22da6488936b98431e8
Joye Nettles <joyejnettles@gmail.com>
2020-02-15 23:48:08 -0500

    Updates

---
+++ precache-manifest.04334f1f76bc33ea1657bc8af11ffacc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4419bade0dcadf69a597f92481f9cf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2fcb14f38a4d3b0a475",
+    "url": "/monsters-rolodex/static/css/main.1009d307.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fcb14f38a4d3b0a475",
+    "url": "/monsters-rolodex/static/js/main.a0f346d0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
